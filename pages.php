2e53581deb3f5cf23a94c63dc4c50b1fb820c4c5
jmanosu <jmanosu@gmail.com>
2018-05-19 17:27:53 -0700

    added account, leaderboard, my champions, arenas

--- pages.php
+++ pages.php
@@ -1,8 +1,9 @@
 <?php
 //$content holds a 2D array with keys being menu names and values being an array with a subtitle, and content
 	$content = array(
-		"Sign Up" => "signUp.php",
-		"List Users" => "listUsers.php",
-		"Login" => "login.php");
+		"Account" => "account.php",
+		"My Champions" => "mychampion.php",
+		"Arenas" => "arenas.php"),
+		"Leaderboard" => "leaderboard.php"),
 
 ?>
