237cb20e76acc72fd497f6703cfd1f3a114080b0
jmanosu <jmanosu@gmail.com>
2018-05-26 19:09:13 -0700

    updated leaderboard

--- leaderboard.php
+++ leaderboard.php
@@ -3,7 +3,7 @@
 <?php
 		$currentpage="Leaderboard";
 		include "pages.php";
-        include "header.php";
+    include "header.php";
 ?>
 <html>
 	<head>
@@ -26,22 +26,33 @@
 
 	$result = mysqli_query($conn, $query);
 	if(!$result){
-		die("Query couldn't complete")
+		die("Query couldn't complete");
 	}
 
 
-	echo "<h1>Sponsors</h1>";
-	echo "<table id='t01' border='t'><tr>"
-	$fields_num = mysqli_num_rows($result);
+	echo "<h1>Leaderboards</h1>";
+	echo "<table id='t01' border='t'><tr>";
+	$fields_num = mysqli_num_fields($result);
+	echo "<td><b>Rank</b></td>";
 	for($i = 0;$i < $fields_num; $i++){
 		$field = mysqli_fetch_field($result);
 		echo "<td><b>$field->name</b></td>";
 	}
 	echo "</tr>\n";
+	$rank = 1;
+	while($row = mysqli_fetch_row($result)) {
+		echo "<tr>";
+		echo "<td>$rank</td>";
+		foreach($row as $cell){
+			echo "<td>$cell</td>";
+		}
+		echo "</tr>\n";
+		$rank += 1;
+	}
 
 	mysqli_free_result($result);
 	mysqli_close($conn);
-	
+
 ?>
  </body>
 </html>
